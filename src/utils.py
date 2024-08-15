1086448ef34da2df06c2c251a900aa4b97b40f10
Konstantin Maurin <artsjesus7@gmail.com>
2024-08-15 23:35:42 +0300

    add function

---
+++ src/utils.py
@@ -0,0 +1,72 @@
+import os
+from configparser import ConfigParser
+import psycopg2
+from config import ROOT_DIR
+
+path = os.path.join(ROOT_DIR, 'database.ini')
+
+
+def connect(filename="database.ini"):
+    '''коннектор для соединения с БД. при вызове можно передать другие аргументы '''
+    config = ConfigParser()
+    config.read(filename)
+    database_config = dict(config.items('database'))
+    conn = psycopg2.connect(
+        host=database_config['host'],
+        database=database_config['database'],
+        user=database_config['user'],
+        password=database_config['password'],
+        port=database_config['port'],
+    )
+    return conn
+
+
+def create_tables(conn):
+    """Создание базы данных и таблиц для сохранения данных о компаниях и вакансиях."""
+
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE employers (
+                    employer_id INT PRIMARY KEY,
+                    employer_name VARCHAR(100) NOT NULL
+                )
+            """)
+
+            cur.execute("""
+                CREATE TABLE vacancies (
+                    vacancy_id SERIAL PRIMARY KEY,
+                    employer_id INT REFERENCES employers(employer_id),
+                    vacancy_name VARCHAR(100) NOT NULL,
+                    salary INT NOT NULL,
+                    currency VARCHAR(100),
+                    requirement TEXT,
+                    vacancy_url VARCHAR(100) NOT NULL
+                )
+            """)
+
+
+def loads_into_table(conn, vacansies: list) -> None:
+    '''заполняет таблицу данными о вакансиях'''
+
+    with conn:
+        with conn.cursor() as cur:
+            for vac in vacansies:
+                cur.execute('INSERT INTO employers (employer_id, employer_name) VALUES '
+                            '(%s,%s)' 'ON CONFLICT (employer_id) DO NOTHING', (vac.employer_id, vac.employer_name))
+
+                cur.execute(
+                    'INSERT INTO vacancies (vacancy_id, employer_id, vacancy_name, salary, currency, '
+                    'requirement, vacancy_url) VALUES '
+                    '(%s,%s,%s,%s,%s,%s, %s)',
+                    (vac.vacancy_id, vac.employer_id, vac.vacancy_name, vac.salary, vac.currency,
+                     vac.requirement, vac.vacancy_url))
+
+
+def drop_table(conn, table_name) -> None:
+    '''удаляет таблицу. по умолчанию соединение закроется. если передать в con_status 1 то останется открытым'''
+
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute(f"DROP TABLE IF EXISTS {table_name}")
+            print(f'из базы данных удалена таблица {table_name}')
